0695334925449028f98358587270fb47fe61bff2
EricksonDutra <ericksond10@gmail.com>
2021-12-07 17:44:49 -0400

    Validation exception

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -103,7 +103,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 	}
 
 	@Override
-	public void onDataChange() {
+	public void onDataChanged() {
 		updateTableView();
 	}
 }
